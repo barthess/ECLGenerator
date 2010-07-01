66dc34febf07749e960df1149d887d3fef44f537
barthess <barthess@yandex.ru>
2010-07-01 16:06:20 +0300

    conflict resolving

--- parse.py
+++ parse.py
@@ -10,11 +10,6 @@ import numpy
 # TODO:
 # remove all double quotes, empty lines, wrong lines
 # adding empty columns if needed
-<<<<<<< HEAD
-=======
-# place Addit field in <<>>
-# replace % by \%
->>>>>>> bash_parser
 #
 # –егул€ркой отделить буквы от номера в позиционном обозначении
 # использовать функцию int() дл€ преобразовани€ строки в число
