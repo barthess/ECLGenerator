0342f838028bcd5456b5d77ce2f9ab53e70f7b2c
barthess <barthess@yandex.ru>
2012-05-15 14:52:15 +0300

    nop

--- htm2csv.sh
+++ htm2csv.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Script for converting from html table to tab separated table
+
 cat $1 | sed 's/<\/b><\/td><td><b>/\t/g' | sed 's/<\/td><td>/\t/g' | sed 's/<tr><td>//' | sed 's/<\/td><\/tr>//' | sed 's/^<b>//' | sed 's/<\/b>$//' | sed '$d' | sed '1,3d'
